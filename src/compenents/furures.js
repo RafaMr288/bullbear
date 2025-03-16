313f5954dbbfc94522426d7334cc87a5718d8bd3
unknown <rafaelmr3453@gmail.com>
2025-03-16 14:58:10 -0300

    atl

--- src/compenents/furures.js
+++ src/compenents/furures.js
@@ -2,7 +2,8 @@ import Img_futures from "../imgs/futures.png"
 import "./futures.css"
 import Img_dex_s from "../imgs/dex-screener.png"
 import Img_bullx from "../imgs/bullx.png"
-import Img_okx from "../imgs/okx2.png"
+import Img_okx from "../imgs/okx.png"
+import Img_orca from "../imgs/orca.png"
 import Header from "./home"
 
 function Futures() {
@@ -13,6 +14,7 @@ function Futures() {
             <img src={Img_dex_s}></img>
             <img src={Img_bullx} onClick={()=>{Header("https://bullx.io/")}}></img>
             <img src={Img_okx}></img>
+            <img src={Img_orca}></img>
         </div>
     )
 
